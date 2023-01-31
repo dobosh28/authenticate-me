abb1bf9c59c96307b5f3fb22e6539e73f4e4b64a
rexqu <junjiequ123@gmail.com>
2023-01-31 15:25:27 -0500

    finished setting up auth part 1

--- authenticate_me/backend/authenticate-me/app/models/user.rb
+++ authenticate_me/backend/authenticate-me/app/models/user.rb
@@ -12,9 +12,38 @@
 #
 class User < ApplicationRecord
   has_secure_password
+  before_validation :ensure_session_token
 
   validates :username, uniqueness: true, length: { in: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message: "can't be an email" }
   validates :email, uniqueness: true, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :session_token, presence: true, uniqueness: true
   validates :password, length: { in: 6..255 }, allow_nil: true
+
+  def self.find_by_credentials(credentials, password)
+    if credentials =~ URI::MailTo::EMAIL_REGEXP
+      user = User.find_by(email: credentials)
+    else
+      user = User.find_by(username: credentials)
+    end
+
+    if user&.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
+
+  def generate_unique_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_unique_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = generate_unique_session_token
+    self.save!
+    self.session_token
+  end
 end
